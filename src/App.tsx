c84ad87e73ad42e05d9618d5d3ba706934bf0303
yueveron <yueveron@gmail.com>
2023-01-13 10:53:51 +0800

    feat: formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+
 import './App.css';
-import DemoZustand from './pages/demo-zustand/DemoZustand';
+// import DemoZustand from './pages/demo-zustand/DemoZustand';
+import DemoFormik from './pages/demo-formik/DemoFormik';
 function App() {
   return (
     <div className="App">
-      <DemoZustand />
+      {/* <DemoZustand /> */}
+      <DemoFormik/>
     </div>
   );
 }
