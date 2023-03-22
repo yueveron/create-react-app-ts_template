40cb6195f44ede3d7cfca815c03de3152c5fe671
yueveron <yueveron@gmail.com>
2023-03-22 14:34:28 +0800

    feat: test axios component

--- src/pages/demo-react-test/__tests__/TextAxios.test.tsx
+++ src/pages/demo-react-test/__tests__/TextAxios.test.tsx
@@ -1,7 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  fireEvent,
+  cleanup,
+  waitFor,
+} from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 import TestAxios from '../pages/TextAxios';
+import axios from 'axios';
+
+afterEach(cleanup);
 
 jest.mock('axios');
 
@@ -13,9 +22,27 @@ test('should display a loading text', async () => {
   expect(loadingEle).toHaveTextContent('Loading...');
 });
 
-// test('should load and display the data', async () => {
-//   const url = '/greeting';
-//   const { getByTestId } = render(<TestAxios url={url} />);
+test('should load and display the data', async () => {
+  const url = '/greeting';
+  const strMsg = 'hello mother';
 
-//   fireEvent.click(getByTestId('fetch-data'));
-// });
+  act(() => {
+    render(<TestAxios url={url} />);
+  });
+
+  const resp = {
+    data: { greeting: strMsg },
+  };
+  // 关键 ：jest.Mock axios.get 劫持被测试代码中的 axios.get 方法
+  const jestMock = axios.get as jest.Mock;
+  jestMock.mockResolvedValue(resp);
+  const btnFetchData = screen.getByTestId('fetch-data');
+  fireEvent.click(btnFetchData);
+
+  expect(jestMock).toHaveBeenCalledTimes(1);
+  expect(jestMock).toHaveBeenCalledWith(url);
+
+  // after clicking to the button, we wait for the greetingData text to be `strMsg`
+  const greetingData = await waitFor(() => screen.getByTestId('show-data'));
+  expect(greetingData).toHaveTextContent(strMsg);
+});
